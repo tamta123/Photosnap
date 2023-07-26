bcfd9d3ddd19d4fcf4ba62434b92a21ab471781e
tamta123 <tamtag186@gmail.com>
2023-07-26 10:21:49 +0400

    wip:redeploy

--- src/views/Features/index.js
+++ src/views/Features/index.js
@@ -1,2 +1,2 @@
 export { default as Features } from "./Features";
-export { default as PhotoSnapFeatures } from "./PhotosnapFeatures";
+export { default as PhotoSnapFeatures } from "./PhotoSnapFeatures";
